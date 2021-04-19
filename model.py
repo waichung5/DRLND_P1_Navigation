a74623aca4bd7989de6fec7057a761f0f23f0434
Lucas Lee <waichung5@users.noreply.github.com>
2021-04-19 14:32:45 +0800

    initial commit

--- model.py
+++ model.py
@@ -2,19 +2,47 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+    
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=64):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
+
 
 class DuelingQNetwork(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=64):
+    def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=64, fc3_units=64):
         super(DuelingQNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, 64)
-        self.value_fc1 = nn.Linear(64, 128)
+        self.fc3 = nn.Linear(fc2_units, fc3_units)
+        
+        self.value_fc1 = nn.Linear(fc3_units, 128)
         self.value_fc2 = nn.Linear(128, 1)
-        self.advantage_fc1 = nn.Linear(64, 128)
+        
+        self.advantage_fc1 = nn.Linear(fc3_units, 128)
         self.advantage_fc2 = nn.Linear(128, action_size)
 
     def forward(self, state):
